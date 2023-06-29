8599a3a056e6f77548eb7852980f95c9d3bfebb4
NicolasKeidong <122652469+NicolasKeidong@users.noreply.github.com>
2023-06-29 11:59:15 -0400

    Add project files.

---
+++ MVC_Music/ViewModels/PerformancesReportVM.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace MVC_Music.ViewModels
+{
+    public class PerformancesReportVM
+    {
+        public int ID {get;set;}
+
+        [Display(Name = "Musician")]
+        public string FormalName
+        {
+            get
+            {
+                return LastName + ", " + FirstName
+                    + (string.IsNullOrEmpty(MiddleName) ? "" :
+                        (" " + (char?)MiddleName[0] + ".").ToUpper());
+            }
+        }
+      
+        public string FirstName { get; set; }
+  
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        [Display(Name = "Total Number of Performances")]
+        public int TotalPerformances { get; set; }
+
+        //[Display(Name = "Total Songs Performanced")]
+        //public int TotalSongs { get; set; }
+
+        [Display(Name = "Average Fee Paid")]
+        [DataType(DataType.Currency)]
+        public double AverageFee { get; set; }
+
+        [Display(Name = "Highest Fee Paid")]
+        [DataType(DataType.Currency)]
+        public double HighestFee { get; set; }
+
+        [Display(Name = "Lowest Fee Paid")]
+        [DataType(DataType.Currency)]
+        public double LowestFee { get; set; }
+    }
+}
